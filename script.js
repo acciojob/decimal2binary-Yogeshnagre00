7765b2a104f1f93b4665de78431608c58cf25526
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-24 22:25:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function threeSum(arr, target) {
-// write your code here
+function decimalToBinary(decimal) {
+  if (decimal === 0) {
+    return '0'; // Special case for decimal 0
+  }
   
+  let binary = '';
+  while (decimal > 0) {
+    binary = (decimal % 2) + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  
+  return binary;
 }
 
+// Example usage
+console.log(decimalToBinary(7)); // Output: 111
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(33)); // Output: 100001
+
+}
 module.exports = threeSum;
