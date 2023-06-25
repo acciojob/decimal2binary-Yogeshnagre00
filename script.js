4233cdd538be91f0aa002c2c22dbf34720c8ecbb
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-06-25 22:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-function decimalToBinary(decimal) {
-  if (decimal === 0) {
-    return '0'; // Special case for decimal 0
-  }
-  
-  let binary = '';
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-  
-  return binary;
+
+function decimalToBinary(num){
+    let binaryStr = ""
+
+    while(num>0){
+
+        let rem = num%2;
+
+        // Method 1 - Appending the new binary character to the front of the string
+        // binaryStr = rem.toString() + binaryStr
+
+        // Method 2 - Append it to the end
+        binaryStr = binaryStr + rem.toString()
+
+        num = Math.floor(num/2)
+    }
+    // Method - 1
+    // return binaryStr
+
+
+    // Method 2- Reverse it
+    return binaryStr.split("").reverse().join("")
 }
 
-// Example usage
-console.log(decimalToBinary(7)); // Output: 111
-console.log(decimalToBinary(10)); // Output: 1010
-console.log(decimalToBinary(33)); // Output: 100001
-
-}
-module.exports = threeSum;
+module.exports = decimalToBinary;
