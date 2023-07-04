6b256c69aea5102ba11ba501f13494d019793c5a
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-07-04 13:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,7 @@
 function decimalToBinary(decimal) {
-  if (decimal === 0) {
-    return '0';
-  }
-
-  let binary = '';
-  while (decimal > 0) {
-    binary = (decimal % 2) + binary;
-    decimal = Math.floor(decimal / 2);
-  }
-
-  return binary;
+  return decimal.toString(2);
 }
-
-// Example usage
-const decimal = 33;
-const binary = decimalToBinary(decimal);
-console.log(binary);
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
